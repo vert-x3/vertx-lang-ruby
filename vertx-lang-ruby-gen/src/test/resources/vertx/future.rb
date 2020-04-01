74cc5f62b8d0cec51bebf37c434af15e95227df4
Julien Viet <julien@julienviet.com>
2020-04-01 18:24:37 +0200

    Regen future

--- vertx-lang-ruby-gen/src/test/resources/vertx/future.rb
+++ vertx-lang-ruby-gen/src/test/resources/vertx/future.rb
@@ -2,6 +2,7 @@
 # This code was copied from generated code from vertx-lang-rb module
 #
 require 'vertx/promise'
+require 'vertx/context'
 require 'vertx/future'
 require 'vertx/util/utils.rb'
 # Generated from io.vertx.core.Future
@@ -21,9 +22,9 @@ module Vertx
       @j_del
     end
     #  Map the failure of a future to a specific <code>value</code>.<p>
-    # 
+    #
     #  When this future fails, this <code>value</code> will complete the future returned by this method call.<p>
-    # 
+    #
     #  When this future succeeds, the result will be propagated to the returned future.
     # @overload otherwise(mapper)
     #   @yield the mapper function
@@ -42,30 +43,6 @@ module Vertx
         raise ArgumentError, "Invalid arguments when calling otherwise(#{args[0]})"
       end
     end
-    #  Set a handler for the result.
-    #  <p>
-    #  If the future has already been completed it will be called immediately. Otherwise it will be called when the
-    #  future is completed.
-    # @overload setHandler(handler)
-    #   @yield the Handler that will be called with the result
-    # @return [self]
-    def set_handler
-      if true
-        if (block_given?)
-          @j_del.java_method(:setHandler, [Java::IoVertxCore::Handler.java_class]).call(block_given? ? Proc.new { |ar| yield(ar.failed ? ar.cause : nil, ar.succeeded ? @j_arg_T.wrap(ar.result) : nil) } : promise)
-          return self
-        else
-          promise = ::Vertx::Util::Utils.promise
-          @j_del.java_method(:setHandler, [Java::IoVertxCore::Handler.java_class]).call(block_given? ? Proc.new { |ar| yield(ar.failed ? ar.cause : nil, ar.succeeded ? @j_arg_T.wrap(ar.result) : nil) } : promise)
-          return ::Vertx::Util::Utils.safe_create(promise.future(),::Vertx::Future, nil)
-        end
-      end
-      if defined?(super)
-        super
-      else
-        raise ArgumentError, "Invalid arguments when calling set_handler()"
-      end
-    end
     #  Handles a failure of this Future by returning the result of another Future.
     #  If the mapper fails, then the returned future will be failed with this failure.
     # @overload recover(mapper)
@@ -81,6 +58,28 @@ module Vertx
         raise ArgumentError, "Invalid arguments when calling recover(#{args[0]})"
       end
     end
+    #  Add a handler to be notified of the result.
+    #  <br/>
+    # @overload onComplete(handler)
+    #   @yield the handler that will be called with the result
+    # @return [self]
+    def on_complete
+      if true
+        if (block_given?)
+          @j_del.java_method(:onComplete, [Java::IoVertxCore::Handler.java_class]).call(block_given? ? Proc.new { |ar| yield(ar.failed ? ar.cause : nil, ar.succeeded ? @j_arg_T.wrap(ar.result) : nil) } : promise)
+          return self
+        else
+          promise = ::Vertx::Util::Utils.promise
+          @j_del.java_method(:onComplete, [Java::IoVertxCore::Handler.java_class]).call(block_given? ? Proc.new { |ar| yield(ar.failed ? ar.cause : nil, ar.succeeded ? @j_arg_T.wrap(ar.result) : nil) } : promise)
+          return ::Vertx::Util::Utils.safe_create(promise.future(),::Vertx::Future, nil)
+        end
+      end
+      if defined?(super)
+        super
+      else
+        raise ArgumentError, "Invalid arguments when calling on_complete()"
+      end
+    end
     #  A Throwable describing failure. This will be null if the operation succeeded.
     # @overload cause()
     # @return [Exception] the cause or null if the operation succeeded.
@@ -94,12 +93,28 @@ module Vertx
         raise ArgumentError, "Invalid arguments when calling cause()"
       end
     end
+    #  Add a handler to be notified of the failed result.
+    #  <br/>
+    # @overload onFailure(handler)
+    #   @yield the handler that will be called with the failed result
+    # @return [self]
+    def on_failure
+      if true
+        @j_del.java_method(:onFailure, [Java::IoVertxCore::Handler.java_class]).call((Proc.new { |event| yield(::Vertx::Util::Utils.from_throwable(event)) unless !block_given? }))
+        return self
+      end
+      if defined?(super)
+        super
+      else
+        raise ArgumentError, "Invalid arguments when calling on_failure()"
+      end
+    end
     #  Map the result of a future to <code>null</code>.<p>
-    # 
+    #
     #  This is a conveniency for <code>future.map((T) null)</code> or <code>future.map((Void) null)</code>.<p>
-    # 
+    #
     #  When this future succeeds, <code>null</code> will complete the future returned by this method call.<p>
-    # 
+    #
     #  When this future fails, the failure will be propagated to the returned future.
     # @overload mapEmpty()
     # @return [::Vertx::Future] the mapped future
@@ -139,11 +154,11 @@ module Vertx
       end
     end
     #  Map the failure of a future to <code>null</code>.<p>
-    # 
+    #
     #  This is a convenience for <code>future.otherwise((T) null)</code>.<p>
-    # 
+    #
     #  When this future fails, the <code>null</code> value will complete the future returned by this method call.<p>
-    # 
+    #
     #  When this future succeeds, the result will be propagated to the returned future.
     # @overload otherwiseEmpty()
     # @return [::Vertx::Future] the mapped future
@@ -175,6 +190,20 @@ module Vertx
         raise ArgumentError, "Invalid arguments when calling failed_future(#{args[0]})"
       end
     end
+    #  Alias for {::Vertx::Future#compose}.
+    # @overload flatMap(mapper)
+    #   @yield
+    # @return [::Vertx::Future]
+    def flat_map(*args)
+      if block_given? && args[0] == nil
+        return ::Vertx::Util::Utils.safe_create(@j_del.java_method(:flatMap, [Java::JavaUtilFunction::Function.java_class]).call((Proc.new { |event| yield(@j_arg_T.wrap(event)).j_del unless !block_given? })),::Vertx::Future, nil)
+      end
+      if defined?(super)
+        super
+      else
+        raise ArgumentError, "Invalid arguments when calling flat_map(#{args[0]})"
+      end
+    end
     #  The result of the operation. This will be null if the operation failed.
     # @overload result()
     # @return [Object] the result or null if the operation failed.
@@ -188,27 +217,33 @@ module Vertx
         raise ArgumentError, "Invalid arguments when calling result()"
       end
     end
-    #  Compose this future with a <code>mapper</code> function.<p>
-    # 
-    #  When this future (the one on which <code>compose</code> is called) succeeds, the <code>mapper</code> will be called with
+    #  Compose this future with a <code>successMapper</code> and <code>failureMapper</code> functions.<p>
+    #
+    #  When this future (the one on which <code>compose</code> is called) succeeds, the <code>successMapper</code> will be called with
     #  the completed value and this mapper returns another future object. This returned future completion will complete
     #  the future returned by this method call.<p>
-    # 
-    #  If the <code>mapper</code> throws an exception, the returned future will be failed with this exception.<p>
-    # 
-    #  When this future fails, the failure will be propagated to the returned future and the <code>mapper</code>
-    #  will not be called.
+    #
+    #  When this future (the one on which <code>compose</code> is called) fails, the <code>failureMapper</code> will be called with
+    #  the failure and this mapper returns another future object. This returned future completion will complete
+    #  the future returned by this method call.<p>
+    #
+    #  If any mapper function throws an exception, the returned future will be failed with this exception.<p>
     # @overload compose(mapper)
     #   @yield the mapper function
+    # @overload compose(successMapper,failureMapper)
+    #   @param [Proc] successMapper the function mapping the success
+    #   @yield the function mapping the failure
     # @return [::Vertx::Future] the composed future
     def compose(*args)
-      if block_given? && args[0] == nil
+      if block_given? && args[0] == nil && args[1] == nil
         return ::Vertx::Util::Utils.safe_create(@j_del.java_method(:compose, [Java::JavaUtilFunction::Function.java_class]).call((Proc.new { |event| yield(@j_arg_T.wrap(event)).j_del unless !block_given? })),::Vertx::Future, nil)
+      elsif args[0].class == Proc && block_given? && args[1] == nil
+        return ::Vertx::Util::Utils.safe_create(@j_del.java_method(:compose, [Java::JavaUtilFunction::Function.java_class,Java::JavaUtilFunction::Function.java_class]).call((Proc.new { |event| args[0].call(@j_arg_T.wrap(event)).j_del unless args[0] == nil }),(Proc.new { |event| yield(::Vertx::Util::Utils.from_throwable(event)).j_del unless !block_given? })),::Vertx::Future, nil)
       end
       if defined?(super)
         super
       else
-        raise ArgumentError, "Invalid arguments when calling compose(#{args[0]})"
+        raise ArgumentError, "Invalid arguments when calling compose(#{args[0]},#{args[1]})"
       end
     end
     #  Create a future that hasn't completed yet and that is passed to the <code>handler</code> before it is returned.
@@ -223,6 +258,18 @@ module Vertx
         super
       else
         raise ArgumentError, "Invalid arguments when calling future()"
+      end
+    end
+    # @overload context()
+    # @return [::Vertx::Context] the context associated with this future or <code>null</code> when
+    def context
+      if !block_given?
+        return ::Vertx::Util::Utils.safe_create(@j_del.java_method(:context, []).call(),::Vertx::Context)
+      end
+      if defined?(super)
+        super
+      else
+        raise ArgumentError, "Invalid arguments when calling context()"
       end
     end
     #  Created a succeeded future with the specified result.
@@ -243,9 +290,9 @@ module Vertx
       end
     end
     #  Map the result of a future to a specific <code>value</code>.<p>
-    # 
+    #
     #  When this future succeeds, this <code>value</code> will complete the future returned by this method call.<p>
-    # 
+    #
     #  When this future fails, the failure will be propagated to the returned future.
     # @overload map(mapper)
     #   @yield the mapper function
@@ -275,6 +322,22 @@ module Vertx
         super
       else
         raise ArgumentError, "Invalid arguments when calling succeeded?()"
+      end
+    end
+    #  Add a handler to be notified of the succeeded result.
+    #  <br/>
+    # @overload onSuccess(handler)
+    #   @yield the handler that will be called with the succeeded result
+    # @return [self]
+    def on_success
+      if true
+        @j_del.java_method(:onSuccess, [Java::IoVertxCore::Handler.java_class]).call((Proc.new { |event| yield(@j_arg_T.wrap(event)) unless !block_given? }))
+        return self
+      end
+      if defined?(super)
+        super
+      else
+        raise ArgumentError, "Invalid arguments when calling on_success()"
       end
     end
     #  Has the future completed?
